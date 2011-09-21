d6e7c1c831219de431216a5c428168e1f3d78d5d
Doug Hathaway <doughathaway@gmail.com>
2011-09-21 10:21:45 -0400

    Only remove Class#to_yaml if it was defined there (fixes bundler-1.0.10 incompatibility).

--- lib/delayed/yaml_ext.rb
+++ lib/delayed/yaml_ext.rb
@@ -15,19 +15,19 @@ class Module
       out.scalar(taguri, self.name, :plain)
     }
   end
-  
+
   def yaml_tag_read_class(name)
     # Constantize the object so that ActiveSupport can attempt
     # its auto loading magic. Will raise LoadError if not successful.
     name.constantize
     name
   end
-  
+
 end
 
 class Class
   yaml_as "tag:ruby.yaml.org,2002:class"
-  remove_method :to_yaml # use Module's to_yaml
+  remove_method :to_yaml if respond_to?(:to_yaml) && method(:to_yaml).owner == Class # use Module's to_yaml
 end
 
 class Struct
